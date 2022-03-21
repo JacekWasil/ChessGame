aa64d1d49c459cff856ab19c58d320c9406e484b
Jacek Wasilewski <jacek.wasilewski95@gmail.com>
2022-03-21 13:43:43 +0100

    fixed bishop bugs

--- BishopMovingRules.py
+++ BishopMovingRules.py
@@ -10,7 +10,7 @@ def bishop_move_enable(bishopfield, destField, chessBoardStatus):
     xMovingDirection = 1 # 1: right, -1: left (a-b-c...)
     yMovingDirection = 1  # 1: up, -1: down (1-2-3...)
     memoryForMovingField = bishopfield
-    if actuallSquareColour == destFieldColour and bishopColour != figureColourOnDestField: #check if destination square is the same colour and figure on dest field is different colour or empty
+    if actuallSquareColour == destFieldColour and bishopColour != figureColourOnDestField and bishopfield[:-1] != destField[:-1]: #check if destination square is the same colour and figure on dest field is different colour or empty
         if int(destField[-1:]) < int(bishopfield[-1:]):
             yMovingDirection = -1
         if int(destField[-1:]) > int(bishopfield[-1:]):
